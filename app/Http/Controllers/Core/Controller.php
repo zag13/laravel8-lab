b43cc8e245e9e7815756308d8a1e36119f50ca61
zs368 <zs8153@163.com>
2020-12-17 16:10:15 +0800

    统一异常处理 && 统一表单验证返回格式

--- app/Http/Controllers/Core/Controller.php
+++ app/Http/Controllers/Core/Controller.php
@@ -5,9 +5,24 @@ namespace App\Http\Controllers\Core;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Validator;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function validate(Request $request, array $rules, array $messages = [], array $customAttributes = [])
+    {
+        $errors = Validator::make($request->all(), $rules, $messages, $customAttributes)->errors()->all();
+
+        if (empty($errors)) {
+            return true;
+        } else {
+            throw new \Exception($errors[0]);
+        }
+    }
+
+
 }
