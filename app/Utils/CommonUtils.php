91e329cd8bb7087d78fc15e6ced5db4246826b84
zs368 <zs8153@163.com>
2020-12-24 18:16:16 +0800

    新增几个有用的方法

--- app/Utils/CommonUtils.php
+++ app/Utils/CommonUtils.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm
  * User: ZS
@@ -22,7 +23,7 @@ class CommonUtils
      * @param string $tmp
      * @return string
      */
-    public static function storageFromUrl($url = '', $tmp = '')
+    public static function storageFromUrl($url = '', $tmp = ''): string
     {
         if (empty($url) || empty($tmp)) throw new \Exception('未传入 url 或 tmp');
         try {
@@ -40,7 +41,7 @@ class CommonUtils
      * @param string $src
      * @return bool
      */
-    public static function rrmDir($src = '')
+    public static function rrmDir($src = ''): bool
     {
         if (empty($src)) return true;
 
@@ -59,4 +60,82 @@ class CommonUtils
         rmdir($src);
     }
 
+    /**
+     * 列表结构转树状结构
+     * @param        $list
+     * @param string $pk
+     * @param string $pid
+     * @param string $child
+     * @param int    $root
+     * @return array
+     */
+    public static function list2tree($list, $pk = 'id', $pid = 'pid', $child = 'children', $root = 0): array
+    {
+        $tree = [];
+        if (!is_array($list)) return $tree;
+
+        // 创建基于主键的数组引用
+        $refer = [];
+        foreach ($list as $key => $value) {
+            $refer[$value[$pk]] = &$list[$key];
+        }
+
+        foreach ($list as $key => $value) {
+            $parentId = $value[$pid];
+            if ($parentId == $root) {
+                $tree[] = &$list[$key];
+            } else {
+                if (isset($refer[$parentId])) {
+                    $parent = &$refer[$parentId];
+                    $parent[$child][] = &$list[$key];
+                }
+            }
+        }
+
+        return $tree;
+    }
+
+    /**
+     * 树状结构转列表结构
+     * @param        $tree
+     * @param string $child
+     * @return array
+     */
+    public static function tree2list($tree, $child = 'children'): array
+    {
+        static $list = [];
+        foreach ($tree as $branch) {
+            $tmp = $branch;
+            unset($tmp[$child]);
+            $list[] = $tmp;
+            if (isset($branch[$child]) && !empty($branch[$child])) {
+                self::tree2list($branch[$child]);
+            }
+        }
+        return $list;
+    }
+
+    /**
+     * 获取对应的子树
+     * @param        $tree
+     * @param        $name
+     * @param string $fileds
+     * @param string $child
+     * @return array|mixed
+     */
+    public static function getSubtree($tree, $name, $fileds = 'name', $child = 'children'): array
+    {
+        $legalTree = [];
+        foreach ($tree as $branch) {
+            if ($branch[$fileds] == $name) {
+                $legalTree = $branch;
+                break;
+            }
+            if (isset($branch[$child])) {
+                $legalTree = self::getSubtree($branch[$child], $name, $fileds, $child);
+                break;
+            }
+        }
+        return $legalTree;
+    }
 }
