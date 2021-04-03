021a8dc8f30c8de7c13f29419f01ccfa0dd74a58
zs368 <zs8153@163.com>
2021-04-03 09:59:41 +0800

    使用 cache 进行优化

--- app/Repos/PostsMongoRepo.php
+++ app/Repos/PostsMongoRepo.php
@@ -11,6 +11,7 @@ namespace App\Repos;
 
 
 use App\Models\Mongos\PostsMongo;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 
 class PostsMongoRepo
@@ -31,14 +32,9 @@ class PostsMongoRepo
     public function getById(string $id, array $columns = ['*'])
     {
         $cacheKey = 'posts_mongo_' . $id;
-        if (Redis::exists($cacheKey)) return unserialize(Redis::get($cacheKey));
-
-        $post = $this->posts->find($id, $columns);
-        // 没有数据就不要缓存（防止数据不同步）
-        if (!$post) return null;
-
-        Redis::setex($cacheKey, 1 * 60 * 60, serialize($post));
-        return $post;
+        return Cache::remember($cacheKey, 1 * 60 * 60, function () use ($id, $columns) {
+            return $this->posts->find($id, $columns);
+        });
     }
 
     /**
@@ -58,26 +54,24 @@ class PostsMongoRepo
     public function addViews(PostsMongo $postsMongo)
     {
         $res = $postsMongo->increment('views');
-        if ($res) Redis::zincrby('popular_posts', 1, $postsMongo->id);
+        if ($res) Redis::zincrby('posts_mongo_popular_', 1, $postsMongo->id);
         return $postsMongo->views;
     }
 
     public function trending($num = 10, $columns = ['*'])
     {
         $cacheKey = 'posts_mongo_popular_' . $num;
-        if (Redis::exists($cacheKey)) return unserialize(Redis::get($cacheKey));
+        return Cache::remember($cacheKey, 10 * 60, function () use ($num, $columns) {
+            $postIds = Redis::zrevrange('posts_mongo_popular_', 0, $num - 1);
+            if (!$postIds) return null;
 
-        $postIds = Redis::zrevrange('popular_posts', 0, $num - 1);
-        if (!$postIds) return null;
-
-        // mongo 没有 order by filed()
-        // 1、用 redis 取出的 id 获取对应的数据后，通过排序字段进行排序
-        // 2、取出相应数据用 PHP 进行排序（只适用于数据较少的情况）
-        // 3、临时给每个 ID 一个权重（用 redis 中的顺序），在 mongo 用该权重进行排序
-        $posts = $this->getByIds($postIds, $columns, function ($query) {
-            return $query->orderBy('view', 'asc');
+            // mongo 没有 order by filed()
+            // 1、用 redis 取出的 id 获取对应的数据后，通过排序字段进行排序
+            // 2、取出相应数据用 PHP 进行排序（只适用于数据较少的情况）
+            // 3、临时给每个 ID 一个权重（用 redis 中的顺序），在 mongo 用该权重进行排序
+            return $this->getByIds($postIds, $columns, function ($query) {
+                return $query->orderBy('view', 'asc');
+            });
         });
-        Redis::setex($cacheKey, 10 * 60, serialize($posts));
-        return $posts;
     }
 }
