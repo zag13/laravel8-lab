a209ad57845cb2f4d289851f8f3f4d16294f600d
zs368 <zs8153@163.com>
2021-04-07 18:50:28 +0800

    新增——自定义规则

---
+++ app/Services/Rules/CustomLimitRule.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class CustomLimitRule implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (php_sapi_name() != 'cli' && $value > 500) return false;
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return '最大查询数量不可超过500条';
+    }
+}
