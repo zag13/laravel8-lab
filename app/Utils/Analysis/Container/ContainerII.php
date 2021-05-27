07f3fcc6f4ea64792101210f571c73cb028be0a7
zs368 <zs8153@163.com>
2021-05-27 17:53:06 +0800

    容器 / 0.5.0 （cp ContainerI.php ContainerII.php）

---
+++ app/Utils/Analysis/Container/ContainerII.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: ZS
+ * Date: 2021/5/19
+ * Time: 2:26 下午
+ */
+
+
+namespace App\Utils\Analysis\Container;
+
+use ArrayAccess;
+use Closure;
+use Exception;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionParameter;
+
+class ContainerII implements ArrayAccess
+{
+    /**
+     * The current globally available container (if any).
+     *
+     * @var static
+     */
+    protected static $instance;
+
+    private $s = [];
+
+    /**
+     * Instantiate a concrete instance of the given type.
+     *
+     * @param $concrete
+     * @return mixed|object
+     *
+     * @throws ReflectionException
+     */
+    public function build($concrete)
+    {
+        if ($concrete instanceof Closure) {
+            return $concrete($this);
+        }
+
+        try {
+            $reflector = new ReflectionClass($concrete);
+        } catch (ReflectionException $e) {
+            throw new Exception("Target class [$concrete] does not exist.", 0, $e);
+        }
+
+        if (!$reflector->isInstantiable()) {
+            throw new Exception("Target [$concrete] is not instantiable.");
+        }
+
+        $constructor = $reflector->getConstructor();
+
+        if (is_null($constructor)) {
+            return new $concrete;
+        }
+
+        $dependencies = $constructor->getParameters();
+
+        try {
+            $instances = $this->resolveDependencies($dependencies);
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+        return $reflector->newInstanceArgs($instances);
+    }
+
+    /**
+     * Resolve all of the dependencies from the ReflectionParameters.
+     *
+     * @param \ReflectionParameter[] $dependencies
+     * @return array
+     *
+     * @throws Exception
+     */
+    protected function resolveDependencies(array $dependencies)
+    {
+        $results = [];
+
+        foreach ($dependencies as $dependency) {
+            $className = Util::getParameterClassName($dependency);
+
+            $result = is_null($className)
+                ? $this->resolvePrimitive($dependency)
+                : $this->build($className);
+
+            if ($dependency->isVariadic()) {
+                $results = array_merge($results, $result);
+            } else {
+                $results[] = $result;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Resolve a non-class hinted primitive dependency.
+     *
+     * @param ReflectionParameter $parameter
+     * @return mixed
+     *
+     * @throws ReflectionException
+     */
+    protected function resolvePrimitive(ReflectionParameter $parameter)
+    {
+        if ($parameter->isDefaultValueAvailable()) {
+            return $parameter->getDefaultValue();
+        }
+
+        throw new Exception("Unresolvable dependency resolving [$parameter] in class {$parameter->getDeclaringClass()->getName()}");
+    }
+
+    /**
+     * Get the globally available instance of the container.
+     *
+     * @return static
+     */
+    public static function getInstance()
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * Determine if a given offset exists.
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function offsetExists($key)
+    {
+        return isset($this->s[$key]);
+    }
+
+    /**
+     * Get the value at a given offset.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function offsetGet($key)
+    {
+        return $this->build($this->s[$key]);
+    }
+
+    /**
+     * Set the value at a given offset.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public function offsetSet($key, $value)
+    {
+        return $this->s[$key] = $value;
+    }
+
+    /**
+     * Unset the value at a given offset.
+     *
+     * @param string $key
+     * @return void
+     */
+    public function offsetUnset($key)
+    {
+        unset($this->s[$key]);
+    }
+
+    /**
+     * Dynamically access container services.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this[$key];
+    }
+
+    /**
+     * Dynamically set container services.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public function __set($key, $value)
+    {
+        $this[$key] = $value;
+    }
+}
