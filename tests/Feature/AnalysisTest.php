9d905ef6434ccb502211a31af8ffc2da35057a91
zs368 <zs8153@163.com>
2021-05-27 18:07:37 +0800

    新增——推荐阅读

--- tests/Feature/AnalysisTest.php
+++ tests/Feature/AnalysisTest.php
@@ -11,15 +11,23 @@ namespace Tests\Feature;
 
 
 use App\Events\UserLoginEvent;
-use App\Models\User;
 use Tests\TestCase;
 
 class AnalysisTest extends TestCase
 {
+    /**
+     * 推荐阅读
+     * https://github.com/LeoYang90/laravel-source-analysis
+     */
+
+    /**
+     * 事件分析文章
+     * https://www.cnblogs.com/alwayslinger/p/13756847.html
+     */
     public function testEvent()
     {
         $this->assertEquals(1, event(new UserLoginEvent(1))[0]);
-        
+
         $this->assertEquals(['a', 'b', 'c'],
             event(new UserLoginEvent(['a', 'b', 'c']))[0]);
     }
