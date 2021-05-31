368e3d6a6f733ed7a6b247ad56de9d0a983a0141
zs368 <zs8153@163.com>
2021-05-31 10:28:03 +0800

    优化下

---
+++ app/Utils/Analysis/Routing/Route.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: ZS
+ * Date: 2021/5/31
+ * Time: 10:18 上午
+ */
+
+
+namespace App\Utils\Analysis\Routing;
+
+
+class Route
+{
+    // TODO
+}
---
+++ tests/Unit/Analysis/ProviderTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: ZS
+ * Date: 2021/5/31
+ * Time: 10:18 上午
+ */
+
+
+namespace Tests\Unit\Analysis;
+
+
+use PHPUnit\Framework\TestCase;
+
+class ProviderTest extends TestCase
+{
+    // TODO
+}
