781529a8476b930e221adbb4d229bb33e4981481
zs368 <zs8153@163.com>
2021-06-15 15:56:06 +0800

    源码分析 — Database （TODO）

---
+++ app/Utils/Analysis/Database/Database.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: ZS
+ * Date: 2021/5/31
+ * Time: 6:14 下午
+ */
+
+
+namespace App\Utils\Analysis\Database;
+
+
+class Database
+{
+    // TODO
+}
---
+++ tests/Unit/Analysis/DatabaseTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: ZS
+ * Date: 2021/5/31
+ * Time: 10:26 上午
+ */
+
+
+namespace Tests\Unit\Analysis;
+
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    public function testData()
+    {
+
+    }
+}
