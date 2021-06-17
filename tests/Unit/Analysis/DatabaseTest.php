38777c9c3011678256e861b68f39382e6addf932
zs368 <zs8153@163.com>
2021-06-17 14:43:24 +0800

    Database / 0.0.1

--- tests/Unit/Analysis/DatabaseTest.php
+++ tests/Unit/Analysis/DatabaseTest.php
@@ -14,8 +14,10 @@ use PHPUnit\Framework\TestCase;
 
 class DatabaseTest extends TestCase
 {
-    public function testData()
-    {
+    // 注册服务、启动连接等自己追下源码就行了
+    // laravel 与 PDO 的交互，看下 src/Illuminate/Database/Connection.php
+    // DB 的一些方法，看下 src/Illuminate/Database/Schema/...
+    // Eloquent 在 DB 上实现了一些有趣的特性，例：修改器（setAttribute）、访问器
+    // 简单的理解 DB 更贴近数据库，权限更高。 Eloquent 的粒度更细，也有了更多实用的方法。
 
-    }
 }
